c116e4e950f7274256ea59465f86f54dfce4045e
Minchan <98478661+chamin966@users.noreply.github.com>
2023-05-26 19:52:58 +0900

    Feat: 인트로 이미지 애니메이션 효과 적용 후 테스트 선택 페이지 표출

--- btwus_webview/src/IntroPage/InitialIntro.tsx
+++ btwus_webview/src/IntroPage/InitialIntro.tsx
@@ -1,5 +1,50 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const InitialIntroContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  animation: 'fade-in' 2s ease-out both;
+
+  @keyframes fade-in {
+    0% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+`;
+
+const InitialIntroImg = styled.img`
+  object-fit: contain;
+  height: 100vh;
+  width: 100vw;
+`;
+
 function InitialIntro() {
-  return <div>InitialIntro</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/');
+    }, 2300);
+  });
+
+  return (
+    <InitialIntroContainer>
+      <InitialIntroImg
+        src='Images/team_logo.jpg'
+        alt='애플리케이션을 제작한 팀의 로고 이미지'
+      />
+    </InitialIntroContainer>
+  );
 }
 
 export default InitialIntro;
